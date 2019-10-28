1e2b70e0818a40286122e7497ea597ad4ecdb071
Sankin Nikita <niksankin@yandex.ru>
2019-10-28 17:21:54 +0300

    Add code formating

--- client/client.c
+++ client/client.c
@@ -1,166 +1,174 @@
 #include "common.h"
 
-void socket_close(int *fd)
+void socket_close(int * fd)
 {
-	if (*fd != -1)
-		close(*fd);
+    if (*fd != -1)
+        close(*fd);
 }
 
-uint64_t ipc_sem_get_info(struct sem_data **semds)
+uint64_t ipc_sem_get_info(struct sem_data ** semds)
 {
-	uint64_t i = 0, maxid;
-	
-	struct sem_data *p;
+    uint64_t i = 0, maxid;
 
-	struct seminfo dummy;
-	union semun arg;
+    struct sem_data * p;
 
-	p = *semds = calloc(1, sizeof(struct sem_data));
-	p->next = NULL;
+    struct seminfo dummy;
+    union semun arg;
 
-	arg.array = (unsigned short *)(void *)&dummy;
-	//maybe need error check here
-	maxid = semctl(0, 0, SEM_INFO, arg);
+    p = *semds = calloc(1, sizeof(struct sem_data));
+    p->next = NULL;
 
-	if (maxid == -1)
-	{
-		perror("semctl SEM_INFO error");
-		free(*semds);
-		return -1;
-	}
+    arg.array = (unsigned short *)(void *)&dummy;
+    // maybe need error check here
+    maxid = semctl(0, 0, SEM_INFO, arg);
 
-	for (int j = 0; j <= maxid; j++) {
-		int semid;
-		struct semid_ds semseg;
-		//struct ipc_perm *ipcp = &semseg.sem_perm;
-		arg.buf = (struct semid_ds *)&semseg;
+    if (maxid == -1)
+    {
+        perror("semctl SEM_INFO error");
+        free(*semds);
+        return -1;
+    }
 
-		semid = semctl(j, 0, SEM_STAT, arg);
-		if (semid < 0) {
-			continue;
-		}
+    for (int j = 0; j <= maxid; j++)
+    {
+        int semid;
+        struct semid_ds semseg;
+        // struct ipc_perm *ipcp = &semseg.sem_perm;
+        arg.buf = (struct semid_ds *)&semseg;
 
-		p->semseg = semseg;
-		p->next = calloc(1, sizeof(struct sem_data));
-		p = p->next;
-		p->next = NULL;
-		i++;
-	}
+        semid = semctl(j, 0, SEM_STAT, arg);
+        if (semid < 0)
+        {
+            continue;
+        }
 
-	if (i == 0)
-		free(*semds);
+        p->semseg = semseg;
+        p->next = calloc(1, sizeof(struct sem_data));
+        p = p->next;
+        p->next = NULL;
+        i++;
+    }
 
-	return i;
+    if (i == 0)
+        free(*semds);
+
+    return i;
 }
 
-struct sem_data_serialized* ipc_sem_serialize(struct sem_data *semds, uint64_t size)
+struct sem_data_serialized * ipc_sem_serialize(struct sem_data * semds,
+                                               uint64_t size)
 {
-	struct sem_data_serialized *sem_arr = calloc(size, sizeof(struct sem_data_serialized));
-	int i = 0;
-	struct passwd * user_data;
+    struct sem_data_serialized * sem_arr =
+        calloc(size, sizeof(struct sem_data_serialized));
+    int i = 0;
+    struct passwd * user_data;
 
-	while (semds)
-	{
-		sem_arr[i].sem_nsems = (uint64_t)semds->semseg.sem_nsems;
-		sem_arr[i].key = (uint64_t)semds->semseg.sem_perm.__key;
-		sem_arr[i].uid = (uint64_t)semds->semseg.sem_perm.uid;
-		sem_arr[i].cuid = (uint64_t)semds->semseg.sem_perm.cuid;
+    while (semds)
+    {
+        sem_arr[i].sem_nsems = (uint64_t)semds->semseg.sem_nsems;
+        sem_arr[i].key = (uint64_t)semds->semseg.sem_perm.__key;
+        sem_arr[i].uid = (uint64_t)semds->semseg.sem_perm.uid;
+        sem_arr[i].cuid = (uint64_t)semds->semseg.sem_perm.cuid;
 
-		user_data = getpwuid(sem_arr[i].uid);
-		strcpy(sem_arr[i].user_name, user_data->pw_name);
+        user_data = getpwuid(sem_arr[i].uid);
+        strcpy(sem_arr[i].user_name, user_data->pw_name);
 
-		semds = semds->next;
-		i++;
-	}
+        semds = semds->next;
+        i++;
+    }
 
-	return sem_arr;
+    return sem_arr;
 }
 
-void ipc_sem_free_info(struct sem_data **semds)
+void ipc_sem_free_info(struct sem_data ** semds)
 {
-	while (*semds) {
-		struct sem_data *next = (*semds)->next;
-		free(*semds);
-		*semds = next;
-	}
+    while (*semds)
+    {
+        struct sem_data * next = (*semds)->next;
+        free(*semds);
+        *semds = next;
+    }
 }
 
-void ipc_sem_serialize_free(struct sem_data_serialized** sem_arr)
+void ipc_sem_serialize_free(struct sem_data_serialized ** sem_arr)
 {
-	free(*sem_arr);
+    free(*sem_arr);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-	struct sockaddr_in serv_addr;
-	int fd ON_SCOPE_EXIT(socket_close);
-	
-	uint64_t sem_size;
+    struct sockaddr_in serv_addr;
+    int fd ON_SCOPE_EXIT(socket_close);
 
-	struct sem_data* sem_data ON_SCOPE_EXIT(ipc_sem_free_info) = NULL;
-	struct sem_data_serialized* sem_serialized ON_SCOPE_EXIT(ipc_sem_serialize_free) = NULL;
+    uint64_t sem_size;
 
-	uint32_t ping = 0xdeadbeef;
-	uint32_t pong;
+    struct sem_data * sem_data ON_SCOPE_EXIT(ipc_sem_free_info) = NULL;
+    struct sem_data_serialized * sem_serialized ON_SCOPE_EXIT(
+        ipc_sem_serialize_free) = NULL;
 
-	char username[32];
+    uint32_t ping = 0xdeadbeef;
+    uint32_t pong;
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(1337);
-	serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    char username[32];
 
-	uint64_t r;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(1337);
+    serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-	if (argc != 2)
-	{
-		printf("Usage ./client USERNAME\n");
-		return 0;
-	}
+    uint64_t r;
 
-	strcpy(username, argv[1]);
+    if (argc != 2)
+    {
+        printf("Usage ./client USERNAME\n");
+        return 0;
+    }
 
-	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-	{
-		perror("socket error");
-		return -1;
-	}
+    strcpy(username, argv[1]);
 
-	sem_size = ipc_sem_get_info(&sem_data);
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        perror("socket error");
+        return -1;
+    }
 
-	if (connect(fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1)
-	{
-		perror("socket error");
-		return -1;
-	}
+    sem_size = ipc_sem_get_info(&sem_data);
 
-	do
-	{
-		send(fd, (const char *)&ping, sizeof(ping), 0);
-		r = recv(fd, (char *)&pong, sizeof(pong), MSG_WAITALL);
-	} while (r == -1);
-	
-	if (send(fd, (const char *)&sem_size, sizeof(sem_size), 0) == -1)
-	{
-		perror("socket error");
-		return -1;
-	}
+    if (connect(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    {
+        perror("socket error");
+        return -1;
+    }
 
-	if (!sem_size)
-		return 0;
+    do
+    {
+        send(fd, (const char *)&ping, sizeof(ping), 0);
+        sleep(1);
+        r = recv(fd, (char *)&pong, sizeof(pong), MSG_WAITALL);
+    } while (r == -1);
 
-	sem_serialized = ipc_sem_serialize(sem_data, sem_size);
+    if (send(fd, (const char *)&sem_size, sizeof(sem_size), 0) == -1)
+    {
+        perror("socket error");
+        return -1;
+    }
 
-	if (send(fd, (const char *)sem_serialized, sem_size * sizeof(struct sem_data_serialized), 0) == -1)
-	{
-		perror("socket error");
-		return -1;
-	}
+    if (!sem_size)
+        return 0;
 
-	if (send(fd, username, sizeof(username), 0) == -1)
-	{
-		perror("socket error");
-		return -1;
-	}
+    sem_serialized = ipc_sem_serialize(sem_data, sem_size);
 
-	return 0;
+    if (send(fd, (const char *)sem_serialized,
+             sem_size * sizeof(struct sem_data_serialized), 0) == -1)
+    {
+        perror("socket error");
+        return -1;
+    }
+
+    if (send(fd, username, sizeof(username), 0) == -1)
+    {
+        perror("socket error");
+        return -1;
+    }
+
+    return 0;
 }
